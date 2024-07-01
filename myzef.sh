f219c188f2388e295d13cc17e8205ef91e6a9be5
Humberto Massa <humbertomassa@gmail.com>
2024-07-01 09:35:31 -0300

    crag and xdg

--- myzef.sh
+++ myzef.sh
@@ -1,2 +1,5 @@
-zef install --/test Readline 'JSON::Class:ver<0.0.21+>' App::Rak::Complete Jupyter::Chatbook Qt::QtWidgets Text::CSV Slang::SQL Prettier::Table App::Mi6 Notify vCard::Parser Term::termios SSH::LibSSH Terminal::LineEditor Term::TablePrint Net::NetRC Slang::SQL Grammar::Tracer TOML YAMLish DBIish Net::IMAP Grammar::Debugger Clu Cro Browser::Open ./Cloud/Git/SSH-LibSSH-Tunnel https://github.com/p6steve/raku-dan-polars.git
-zef install --/test --exclude=007 --exclude=p6doc https://github.com/massa/Task-Massa.git
+cd
+nice ionice zef install --serial --/test --exclude=Bailador --exclude=007 --exclude=p6doc Readline 'JSON::Class:ver<0.0.21+>' Grammar::Tracer Term::termios
+nice ionice zef install --serial --/test --exclude=Bailador --exclude=007 --exclude=p6doc Readline 'JSON::Class:ver<0.0.21+>' Grammar::Tracer App::Rak::Complete Jupyter::Chatbook Qt::QtWidgets Text::CSV Slang::SQL Prettier::Table App::Mi6 Notify vCard::Parser Term::termios SSH::LibSSH Terminal::LineEditor Term::TablePrint Net::NetRC Slang::SQL TOML YAMLish DBIish Net::IMAP Grammar::Debugger Clu Cro Browser::Open ./Cloud/Git/SSH-LibSSH-Tunnel https://github.com/p6steve/raku-dan-polars.git
+nice ionice zef install --serial --/test --exclude=Bailador --exclude=007 --exclude=p6doc https://github.com/massa/Task-Massa.git
+nice ionice zef install --serial --/test --exclude=Bailador --exclude=007 --exclude=p6doc https://github.com/massa/Task-Massa.git
