007044bd0e82182914b91ecb6b838ee02656ca25
Humberto Massa <humbertomassa@gmail.com>
2025-04-01 20:14:45 -0300

    mais alguns itens

--- myzef.sh
+++ myzef.sh
@@ -1,5 +1,5 @@
 cd
-nice ionice zef install --serial --/test --exclude=Bailador --exclude=007 --exclude=p6doc Readline 'JSON::Class:ver<0.0.21+>' Grammar::Tracer Term::termios
-nice ionice zef install --serial --/test --exclude=Bailador --exclude=007 --exclude=p6doc Readline 'JSON::Class:ver<0.0.21+>' Grammar::Tracer App::Rak::Complete Jupyter::Chatbook Qt::QtWidgets Text::CSV Slang::SQL Prettier::Table App::Mi6 Notify vCard::Parser Term::termios SSH::LibSSH Terminal::LineEditor Term::TablePrint Net::NetRC Slang::SQL TOML YAMLish DBIish Net::IMAP Grammar::Debugger Clu Cro Browser::Open ./Cloud/Git/SSH-LibSSH-Tunnel https://github.com/p6steve/raku-dan-polars.git
-nice ionice zef install --serial --/test --exclude=Bailador --exclude=007 --exclude=p6doc https://github.com/massa/Task-Massa.git
-nice ionice zef install --serial --/test --exclude=Bailador --exclude=007 --exclude=p6doc https://github.com/massa/Task-Massa.git
+nice ionice zef install --serial --/test --exclude=sqlite3 --exclude=Bailador --exclude=007 --exclude=p6doc Readline 'JSON::Class:ver<0.0.21+>' Grammar::Tracer Term::termios
+nice ionice zef install --serial --/test --exclude=sqlite3 --exclude=Bailador --exclude=007 --exclude=p6doc Readline 'JSON::Class:ver<0.0.21+>' Grammar::Tracer App::Rak::Complete Jupyter::Chatbook Qt::QtWidgets Text::CSV Slang::SQL Prettier::Table App::Mi6 Notify vCard::Parser Term::termios SSH::LibSSH Terminal::LineEditor Term::TablePrint Net::NetRC Slang::SQL TOML YAMLish DBIish Net::IMAP Grammar::Debugger Clu Cro Browser::Open https://github.com/massa/Task-Massa.git https://github.com/p6steve/raku-dan-polars.git
+nice ionice zef install --serial --/test --exclude=sqlite3 --exclude=Bailador --exclude=007 --exclude=p6doc https://github.com/massa/Task-Massa.git
+nice ionice zef install --serial --/test --exclude=sqlite3 --exclude=Bailador --exclude=007 --exclude=p6doc https://github.com/massa/Task-Massa.git
